3caeb64c5644db4aae9def7da538f51fa2ee464c
Émilie Aubin <emilie.aubin@univ-perp.fr>
2019-03-12 16:38:18 +0100

    Générer une liste de 10 nucléotides

--- test_genome.py
+++ test_genome.py
@@ -7,7 +7,20 @@ def main():
 	ma_liste = []
 	for i in range(10):
 		ma_liste.append(random.randint(0, 3))
-	print(ma_liste)
+	print(affichage(ma_liste))
+
+def affichage(une_liste):
+	resultat = []
+	for i in range(len(une_liste)):
+		if (une_liste[i] == 0):
+			resultat.append("A")
+		elif (une_liste[i] == 1):
+			resultat.append("C")
+		elif (une_liste[i] == 2):
+			resultat.append("G")
+		elif (une_liste[i] == 3):
+			resultat.append("T")
+	return resultat
 
 if __name__ == "__main__":
 	main()
